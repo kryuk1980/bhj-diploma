849719fa6be0e0ce487a2e0ec98a3df64e48dc96
kryuk1980 <kruyk@bk.ru>
2023-06-01 21:38:45 +0300

    Выполнено

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,20 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+const createRequest = (options) => {
+    const xhr = new XMLHttpRequest;
 
-};
+    xhr.responseType = 'json';
+    
+    try {
+        xhr.open(options.method, options.url);
+        xhr.send(options.data);
+    }
+    catch (e) {
+        console.log('Ошибка при передаче запроса');
+    }
+
+    xhr.onload = () => {
+        options.callback(xhr.err, xhr.response);
+    }
+}
