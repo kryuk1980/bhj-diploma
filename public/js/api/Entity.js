78678390cca4de41751996efe3a7fb734e53177e
kryuk1980 <kruyk@bk.ru>
2023-06-01 21:38:58 +0300

    Выполнено

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -3,13 +3,20 @@
  * Имеет свойство URL, равно пустой строке.
  * */
 class Entity {
+  static URL = '';
   /**
    * Запрашивает с сервера список данных.
    * Это могут быть счета или доходы/расходы
    * (в зависимости от того, что наследуется от Entity)
    * */
-  static list(data, callback){
-
+  static list(data, callback) {
+    const url = this.URL === '/transaction' ? this.URL + '?account_id=' + data.account_id : this.URL + '?' + data;
+    createRequest({
+      url,
+      method: 'GET',
+      data,
+      callback
+    });
   }
 
   /**
@@ -18,7 +25,12 @@ class Entity {
    * что наследуется от Entity)
    * */
   static create(data, callback) {
-
+    createRequest({
+      url: this.URL,
+      method: 'PUT',
+      data,
+      callback
+    });
   }
 
   /**
@@ -26,6 +38,11 @@ class Entity {
    * (в зависимости от того, что наследуется от Entity)
    * */
   static remove(data, callback ) {
-
+    createRequest({
+      url: this.URL,
+      method: 'DELETE',
+      data,
+      callback
+    });
   }
 }
